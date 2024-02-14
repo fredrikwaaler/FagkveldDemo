747513955d578a7beb5b63cd980c8ee2a592efdc
fredrikwaaler <fredrik.waaler@gmail.com>
2024-02-14 15:01:53 +0100

    Initial Commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import json
+
+def read_data_from_json(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
+
+def main():
+    # TODO: Someone implement this
+    return
+
+if __name__ == "__main__":
+    main()
